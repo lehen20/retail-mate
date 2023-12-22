a7ca54fcdb3437a404ab37a0ca1f3a0c0126b9e3
lehen20 <zehralehen@gmail.com>
2023-12-22 17:50:21 +0530

    sign-up ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import About from './pages/About.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Profile from './pages/Profile.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
