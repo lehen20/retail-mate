17dad389edaa8e05238a086d013ec3b74bb3ed90
lehen20 <zehralehen@gmail.com>
2024-01-01 17:55:38 +0530

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,20 +1,21 @@
 // import React from 'react'
 import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const[error, setError] = useState(false);
-  const[loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state)=> state.user);
   const navigate= useNavigate();
+  const dispatch= useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value});
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true)
-      setError(false);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',{
         method: 'POST',
         headers: {
@@ -24,14 +25,15 @@ export default function SignIn() {
 
       });
       const data = await res.json();
-      setLoading(false);
+      // const error=data.error;
       if(data.success === false){
-        setError(true);
+        console.log(data.error);
+        dispatch(signInFailure(data.error));
         return;}
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch(error){
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
     }
     // } catch(error){
     //   setLoading(false)
@@ -65,7 +67,10 @@ export default function SignIn() {
           <span className='text-blue-500'> Sign Up </span>
         </Link>
       </div>
-      <p className='text-red-700 mt-5'> {error && 'Something went wrong'} </p>
+      <p className='text-red-700 mt-5'> 
+        {error ? error || 'Something went wrong' : ""}
+        {/* {error && 'Something is wrong'} */}
+      </p>
     </div>
   );
     
